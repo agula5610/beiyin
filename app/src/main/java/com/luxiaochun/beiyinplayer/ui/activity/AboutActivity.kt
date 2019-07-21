b3b0dbeeb5f9e62d99a4d981ea8a6f7fa16148d4
yunlu0831 <237489473@qq.com>
2019-07-21 13:49:07 +0800

    各种初始化

---
+++ app/src/main/java/com/luxiaochun/beiyinplayer/ui/activity/AboutActivity.kt
@@ -0,0 +1,10 @@
+package com.luxiaochun.beiyinplayer.ui.activity
+
+import com.luxiaochun.beiyinplayer.R
+import com.luxiaochun.beiyinplayer.base.BaseActivity
+
+class AboutActivity : BaseActivity(){
+    override fun getLayoutId(): Int {
+        return R.layout.activity_about
+    }
+}
--- app/src/main/java/com/luxiaochun/beiyinplayer/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/luxiaochun/beiyinplayer/ui/fragment/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.luxiaochun.beiyinplayer.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.preference.Preference
 import android.preference.PreferenceFragment
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.luxiaochun.beiyinplayer.R
+import com.luxiaochun.beiyinplayer.ui.activity.AboutActivity
 
 class SettingFragment : PreferenceFragment(){
 
@@ -18,7 +20,7 @@ class SettingFragment : PreferenceFragment(){
 
     override fun onPreferenceTreeClick(preferenceScreen: PreferenceScreen?, preference: Preference?): Boolean {
         when (preference?.key){
-            "about"->{}
+            "about"->{activity.startActivity(Intent(activity,AboutActivity::class.java))}
 
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference)
